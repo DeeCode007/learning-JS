e66dedd49dde562f82a81fd9fb6132d5bf3067c5
Deecode007 <dgehlod180@gmail.com>
2023-09-12 18:41:44 +0530

    adding two more files

---
+++ 01_basics/05_string.js
@@ -0,0 +1,12 @@
+// how to use single quote, double quote and a backlash in a string data type
+
+// let string = "I am learning 'javascript'"
+// let string1 = "I am learning \'javascript'\ "
+// let string2 = "I am learning \"javascript\" "
+// let string4 = "this is called \\ backlash character" //output = this is called \ backlash character
+// console.log(string)
+
+// string is primitive data type but there is a string object too in java script we can create it by using 'new' keyword and string() constructor. after creating a string object, we can use its prototype methods like charat,split,indexof.
+
+let stringObj = new String(I am learning Javascript)
+console.log(stringObj)
